24ae43cdc752ef2745bbb92abc49927cc622340d
Oliver Jordy Pérez Escamilla <oliverjordyperezescamilla@MacBook-Pro.local>
2020-10-11 16:29:32 -0500

    [Feature][AlbumCollectionModule] Add state machine to view model
    * [X] Add PhotoCollectionsState machine
    * [X] Group downloaded photos into a dicctionary

--- TypicodePhotoCollection/TypicodePhotoCollection/Modules/AlbumCollection/ViewModel/AlbumCollectionViewModel.swift
+++ TypicodePhotoCollection/TypicodePhotoCollection/Modules/AlbumCollection/ViewModel/AlbumCollectionViewModel.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import RxSwift
+import RxCocoa
 
 final class AlbumCollectionViewModel: AlbumCollectionViewModelProtocol {
     
@@ -16,6 +17,7 @@ final class AlbumCollectionViewModel: AlbumCollectionViewModelProtocol {
     // MARK: - Private Properties
     private let remoteApiClient: AlbumColletionRemoteApiClientProtocol
     private let disposeBag = DisposeBag()
+    let photoCollectionsState = BehaviorRelay<PhotoCollectionsState>(value: .loading)
     
     // MARK: - Initializers
     init(remoteApiClient: AlbumColletionRemoteApiClientProtocol) {
@@ -28,14 +30,31 @@ final class AlbumCollectionViewModel: AlbumCollectionViewModelProtocol {
     }
     
     private func getPhotoColletion() {
+        photoCollectionsState.accept(.loading)
         remoteApiClient
             .get(type: [PhotoDetail].self, from: Endpoint.photoCollections)
-            .subscribe { photoDetails in
-                print("PhotoDetail object count: \(photoDetails.count)")
-            } onError: { error in
-                print("\(String(describing: error))")
+            .map (groupPhotosByAlbumId)
+            .subscribe { [weak self] photoCollections in
+                self?.photoCollectionsState.accept(.ready(photoCollections))
+            } onError: { [weak self] error in
+                self?.photoCollectionsState.accept(.error(error.localizedDescription))
             }
             .disposed(by: disposeBag)
     }
 
+    private func groupPhotosByAlbumId(photoDetails: [PhotoDetail]) -> [Int: [PhotoDetail]] {
+        photoDetails.reduce(into: [:]) { result, next in
+            guard result[next.albumId] != nil else { return result[next.albumId] = [next] }
+            result[next.albumId]? += [next]
+        }
+    }
+}
+
+// MARK: - AlbumCollectionViewModel PhotoCollectionState extension
+extension AlbumCollectionViewModel {
+    enum PhotoCollectionsState {
+        case loading
+        case ready([Int: [PhotoDetail]])
+        case error(String)
+    }
 }
